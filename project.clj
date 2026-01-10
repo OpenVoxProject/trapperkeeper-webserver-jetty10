5a1f1e57852f3ab83e3e6e2b8c349236110b4b9c
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-10 00:36:49 +0000

    Version 1.1.1

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 
 (fail-if-logback->1-3! logback-version)
 
-(defproject org.openvoxproject/trapperkeeper-webserver-jetty10 "1.1.1-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-webserver-jetty10 "1.1.1"
   :description "A jetty10-based webserver implementation for use with the org.openvoxproject/trapperkeeper service framework."
   :url "https://github.com/openvoxproject/trapperkeeper-webserver-jetty10"
   :license {:name "Apache License, Version 2.0"
